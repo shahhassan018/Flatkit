399013478e3289b771e9b1a2b851160bc7b4f529
Syed Shah Hassan <shah.hassan@brainplow.com>
2018-02-13 16:28:07 +0500

    My first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+  
+])// HomeComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
